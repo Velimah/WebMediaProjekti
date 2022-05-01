ede8f3dd412e30123e3c3fbd5fc664defddae0b3
velimah <veli-matti.heino@metropolia.fi>
2022-05-01 19:24:39 +0300

    darktheme muisti ja kaikille sivuille

--- js/teema.js
+++ js/teema.js
@@ -4,16 +4,30 @@
     element.classList.toggle("dark-mode");
    
 }*/
+const themeMemory = localStorage.getItem('theme');
 
 const theme = document.querySelector('#theme-link');
 
+if (themeMemory === 'vaalea') {
+    theme.href = 'css/style.css';
+}
+if (themeMemory === 'tumma') {
+    theme.href = 'css/tumma.css';
+}
+
 function darkMode(){
 
-    if (theme.getAttribute('href') == 'css/style.css'){
+    if (theme.getAttribute('href') === 'css/style.css'){
         theme.href = 'css/tumma.css';
+        localStorage.setItem('theme', 'tumma');
+
 
     } else {
         theme.href = 'css/style.css';
+        localStorage.setItem('theme', 'vaalea');
+
     }
 
 }
+
+
