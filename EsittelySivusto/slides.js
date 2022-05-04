f596eaac35fe7847ac4ae4fe363545b67f63affb
velimah <veli-matti.heino@metropolia.fi>
2022-05-04 19:41:59 +0300

    räjäytyksii.

--- EsittelySivusto/slides.js
+++ EsittelySivusto/slides.js
@@ -15,13 +15,17 @@ function showSlides(n) {
   let slides = document.querySelectorAll(".slidesShow");
 
 
-  if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
-  
-	for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  
-	slides[slideIndex-1].style.display = "block";
- 
+
+  slides[slideIndex - 1].style.display = "block";
+
 }
